9dd1f2ad1b3d017ea1d39dcbe37e8a32b766f188
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-08-01 00:35:36 +0900

    x-api-keyを付与したラッパーを追加

--- fetchi.ts
+++ fetchi.ts
@@ -11,3 +11,18 @@ export const useFetchi = async <T>(url: string, opts?: object) => {
     data: result.data as globalThis.Ref<NonNullable<typeof result.data.value>>,
   }
 }
+
+/**
+ * x-api-keyを付与した$fetch
+ *
+ * @param url
+ * @param opts
+ * @returns
+ */
+export const $fetchByApiKey = async <T>(url: string, opts?: object) => {
+  const runtimeConfig = useRuntimeConfig()
+  await $fetch<T>(url, {
+    ...opts,
+    headers: { 'x-api-key': runtimeConfig.public.apiKey },
+  })
+}
